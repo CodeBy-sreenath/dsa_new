28a016f1b6558dc875368d9fedb7cdd8655bc37a
CodeBy-sreenath <sreenathts655@gmail.com>
2026-07-22 20:07:16 +0530

    balanced paranthesis

---
+++ Infosys/Stack/balanced_par.py
@@ -0,0 +1,25 @@
+exp=input("enter the expression")
+stack=[]
+pairs={
+    ')':'(',
+    '}':'{',']':'['
+}
+balanced=True
+for ch in exp:
+    if ch in '({[':
+        stack.append(ch)
+    elif ch in ')}]':
+        if len(stack)==0:
+            balanced=False
+            break
+        top=stack.pop()
+        if top!=pairs[ch]:
+            balanced=False
+            break
+if len(stack)!=0:
+    balanced=False
+            
+if balanced:
+    print("balanced")
+else:
+    print("not balanced")        
