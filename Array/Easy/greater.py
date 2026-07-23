753d321707109d8a2f877647e8b4fa5bcb9c913f
CodeBy-sreenath <sreenathts655@gmail.com>
2026-07-23 16:34:20 +0530

    smallest

--- Array/Easy/greater.py
+++ Array/Easy/greater.py
@@ -1,13 +1,13 @@
 a=int(input("enter the first number"))
 b=int(input("enter the second number"))
 c=int(input("enter the third number"))
-if a>b:
-    if a>c:
-        print("larger is",a)
+if a<b:
+    if a<c:
+        print(a,"is smaller")
     else:
-        print("larger is",c)
+        print(c,"is smaller")
 else:
-    if b>c:
-        print("larger is",b)
+    if b<c:
+        print(b,"is smaller")
     else:
-        print("larger is",c)                
+        print(c,"is smaller")                
