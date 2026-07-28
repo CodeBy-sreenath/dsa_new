00308854ce8bf985a303d4b368159f2fb6b476ae
CodeBy-sreenath <sreenathts655@gmail.com>
2026-07-28 19:37:41 +0530

    sorting functions

--- Array/Easy/builtin_functions.py
+++ Array/Easy/builtin_functions.py
@@ -1,8 +1,10 @@
-numbers=[10,20,30,40,50]
+numbers=[10,20,30,40,50,12,25,50,78,0,34]
 numbers.append(60)
 
 numbers.extend([10])
-numbers.insert(0,5)
-numbers.remove(5)
-numbers.pop(0)
+numbers.insert(3,5)
 print(numbers)
+print(numbers.count(10))
+print(numbers.index(50,6,11))
+sot=sorted(numbers)
+print(sot)
