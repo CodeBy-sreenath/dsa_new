ef0a44f4f28f07f7626815b104e25f7793068f8f
CodeBy-sreenath <sreenathts655@gmail.com>
2026-08-01 19:16:08 +0530

    contain duplicates

---
+++ Array/Easy/duplicate_contain.py
@@ -0,0 +1,8 @@
+def duplicate(nums):
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]==nums[j]:
+                return True
+    return False
+nums=[1,2,1] 
+print(duplicate(nums))       
