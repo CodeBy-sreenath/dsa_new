5f3f4d0df57931feb2c570ec1a9996887cdc53bb
CodeBy-sreenath <sreenathts655@gmail.com>
2026-08-03 20:22:49 +0530

    dictionary methods

--- Array/Easy/dict.py
+++ Array/Easy/dict.py
@@ -6,3 +6,13 @@ details={
 print(details)
 print(details["name"])
 print(details.get('age'))
+for item in details.values():
+    print(item)
+for item in details.keys():
+    print(item)
+for item in details.items():
+    print(item)
+print(details.pop('name')) 
+print(details)  
+del details["age"]
+print(details)         
